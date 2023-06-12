48d95ebe99a7b0e7add330609fb34acd43d2a869
xlsymphonylx <xlsymphonylx@gmail.com>
2023-06-11 21:08:12 -0600

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -10,11 +12,20 @@ const authRoutes = require("./routes/authRoutes");
 const protectedRoutes = require("./routes/protectedRoutes");
 const bankAccountRoutes = require("./routes/bankAccountRoutes");
 const requestRoutes = require("./routes/requestRoutes");
+const creditCardRoutes = require("./routes/creditCardRoutes");
+const requestStateRoutes = require("./routes/requestStateRoutes");
+const requestTypeRoutes = require("./routes/requestTypeRoutes");
 
 app.use("/auth", authRoutes);
 app.use("/protected", protectedRoutes);
 app.use("/bankAccount", bankAccountRoutes);
 app.use("/request", requestRoutes);
+app.use("/creditcard", creditCardRoutes);
+app.use("/requestState", requestStateRoutes);
+app.use("/requestType", requestTypeRoutes);
+
+
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
